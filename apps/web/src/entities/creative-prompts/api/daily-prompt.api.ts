37c1833a4b1998333cb4d69c3b6bc008afde6dd8
Kalley Powell <kalley.powell@gmail.com>
2025-09-27 17:14:21 -0500

    Use cloudflare prompt server

--- apps/web/src/entities/creative-prompts/api/daily-prompt.api.ts
+++ apps/web/src/entities/creative-prompts/api/daily-prompt.api.ts
@@ -2,7 +2,9 @@ import type { DailyPrompt } from "../model/daily-prompt.schema";
 
 export async function fetchDailyPrompt() {
 	try {
-		const response = await fetch("/estrange/data/daily-prompt.json");
+		const response = await fetch(
+			"https://estrange-prompt-server.kalley-powell.workers.dev/prompt",
+		);
 		return response.json() as unknown as DailyPrompt;
 	} catch (error) {
 		console.error("Error fetching prompt:", error);
